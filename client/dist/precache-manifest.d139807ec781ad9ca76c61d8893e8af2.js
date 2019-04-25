e9c4219e2539c67525c8c0fb8f3f41598e9b1e78
Jackson Yang <jacksony@gmail.com>
2019-04-25 16:07:22 +0800

    dist commit

---
+++ client/dist/precache-manifest.d139807ec781ad9ca76c61d8893e8af2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "95fef60db00fbb1dec63",
+    "url": "/js/chunk-vendors.47350325.js"
+  },
+  {
+    "revision": "a76fb7fe8371ae794ac7",
+    "url": "/js/app.b12b7b11.js"
+  },
+  {
+    "revision": "e3265da0b912786c641c",
+    "url": "/js/about.69260e02.js"
+  },
+  {
+    "revision": "8eb68d7581085dcd3bd997aa5a90c1b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a76fb7fe8371ae794ac7",
+    "url": "/css/app.dde1df84.css"
+  }
+];
